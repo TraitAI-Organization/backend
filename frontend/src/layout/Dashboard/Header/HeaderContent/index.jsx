b73b4f38f60cfb3aae72131ecd9a12df6fdbff2f
JaccobStanton <jaccobstanton@gmail.com>
2026-04-22 08:54:45 -0500

    Unused notifications icon commented out for demo purposes

--- frontend/src/layout/Dashboard/Header/HeaderContent/index.jsx
+++ frontend/src/layout/Dashboard/Header/HeaderContent/index.jsx
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 // project imports
 import Search from './Search';
 import Profile from './Profile';
-import Notification from './Notification';
+// import Notification from './Notification';
 import MobileSection from './MobileSection';
 
 // ==============================|| HEADER - CONTENT ||============================== //
@@ -17,7 +17,7 @@ export default function HeaderContent() {
     <>
       {!downLG && <Search />}
       {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
-      <Notification />
+      {/* <Notification /> */}
       {!downLG && <Profile />}
       {downLG && <MobileSection />}
     </>
